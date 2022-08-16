f05d128fb447783e72d0505fba56690c502c5045
BenjaminLemons <106608948+BenjaminLemons@users.noreply.github.com>
2022-08-16 14:05:00 -0500

    Add files via upload

--- digitalclock.js
+++ digitalclock.js
@@ -11,12 +11,15 @@ class DigitalClock {
 
     update() {
         const parts = this.getTimeParts();
-        const minuteFormatted = parts.minute.toString().padStart(2, "0");
-        const timeFormatted = `${parts.hour}:${parts.minuteFormatted}`;
+        //var minuteFormatted = parts.minute.toString().padStart(2, "0");
+        const timeFormatted = `${parts.hour}:${parts.minute}`;
+        //const timeFormatted = `${parts.hour}:${parts.minute.toString().padStart(2, "0")}`;
         const amPM = parts.isAm ? 'AM' : "PM";
 
         this.element.querySelector(".clocktime").textContent = timeFormatted;
         this.element.querySelector(".clockampm").textContent = amPM;
+
+        console.log(timeFormatted);
 
     }
 
@@ -25,7 +28,8 @@ class DigitalClock {
 
         return {
             hour: now.getHours() % 12 || 12,
-            minute: now.getMinutes(),
+            minute: now.getMinutes().toString().padStart(2, "0"),
+            //minute: now.getMinutes(),
             isAM: now.getHours() < 12
         };
     }
